5f48244044fc73f0ac6143c54074cd3026fbc96e
oggysy <ogisoys@gmail.com>
2023-01-30 00:22:32 +0900

    initial commit

---
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        guard let buttonTitle = sender.currentTitle else { return }
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        guard let buttonTitleAsAnswer = Double(buttonTitleMinusPercentSign) else { return }
+        tip = buttonTitleAsAnswer / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        guard let bill = billTextField.text else { return }
+        guard bill != "" else { return }
+        guard let billTotal = Double(bill) else { return }
+        let result = billTotal * (1.0 + tip) / Double(numberOfPeople)
+        finalResult = String(format: "%.2f", result)
+        
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "goToResults", let destinationVC = segue.destination as? ResultsViewController else { return }
+        destinationVC.result = finalResult
+        destinationVC.tip = Int(tip * 100)
+        destinationVC.split = numberOfPeople
+        
+    }
+}
+
